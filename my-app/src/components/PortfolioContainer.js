214407554a6704f6496375135c2ea289d6e635b6
unknown <youssef.ojeil@hotmail.com>
2022-12-05 18:59:59 -0500

    setup navbar and templates for each page, created Header, Footer & Project Components

--- my-app/src/components/PortfolioContainer.js
+++ my-app/src/components/PortfolioContainer.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './Home';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Resume from './Resume';
+import Home from './Pages/Home';
+import Portfolio from './Pages/Portfolio';
+import Contact from './Pages/Contact';
+import Resume from './Pages/Resume';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
     // initial state of currentpage set to home
