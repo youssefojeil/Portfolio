f0435d0fe099887ddea65c73cf0fbee20ddaf181
unknown <youssef.ojeil@hotmail.com>
2022-12-05 23:59:07 -0500

    create second project component with different styling for testing

--- my-app/src/components/Pages/Portfolio.js
+++ my-app/src/components/Pages/Portfolio.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Project from '../Project';
+import ProjectTest from '../Project2'
 
 export default function Portfolio() {
   return (
     <div>
-      <Project/>
+      <ProjectTest/>
     </div>
   );
 }
--- my-app/src/components/Project2.js
+++ my-app/src/components/Project2.js
@@ -12,26 +12,29 @@ export default function Project() {
                     <a href='https://github.com/joseobm92/MVC-cryptofolio' target='_blank' rel="noopener noreferrer"> 
                         <img src={cryptofolio} className="d-block w-100 " alt="..."/>
                     </a>
-                    <h3 className='centered-title black-text'> Cryptofolio </h3>
-                    <p className='centered-text black-text'> A cryptocurrency portfolio tracker geared towards beginners</p>
+                    <div className="d-none d-md-block black-text carousel-caption-custom">
+                        <h5 className='black-text'>Cryptofolio</h5>
+                        <p className='black-text'> A cryptocurrency portfolio tracker geared towards beginners</p>
+                    </div>
                 </div>
                 <div className="carousel-item carousel-img">
                     <a href='https://joseobm92.github.io/Travel-Buddy/' target='_blank' rel="noopener noreferrer"> 
                         <img src={travelbuddy} className="d-block w-100 " alt="..."/>
-                        
                     </a>
-                    <div>
-                        
+                    <div className="d-none d-md-block carousel-caption-custom">
+                        <h3> Travel Buddy </h3>
+                        <p> An app that tracks flight status and weather data, built for travelers</p>
                     </div>
-                    <h3 className='centered-title'> Travel Buddy </h3>
-                    <p className='centered-text'> An app that tracks flight status and weather data, built for travelers</p>
+                    
                 </div>
                 <div className="carousel-item carousel-img">
                     <a href='https://www.lostboy.io/' target='_blank' rel="noopener noreferrer"> 
                         <img src={lostboy} className="d-block w-100 " alt="..."/>
                     </a>
-                    <h3 className='centered-title'> LOSTBOY </h3>
-                    <p className='centered-text'>A lifestyle brand utilzing music & web3 technology to raise Mental Health Awareness</p>    
+                    <div className="d-none d-md-block carousel-caption-custom">
+                        <h3> LOSTBOY </h3>
+                        <p>A lifestyle brand utilzing music & web3 technology to raise Mental Health Awareness</p>    
+                    </div>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
