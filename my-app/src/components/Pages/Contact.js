e956b99bb80bde666cb199658e319f628526633c
unknown <youssef.ojeil@hotmail.com>
2022-12-07 18:39:38 -0500

    setup form in contact me page

--- my-app/src/components/Pages/Contact.js
+++ my-app/src/components/Pages/Contact.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className='container'>
+      <h1>Contact Me</h1>
+      <div className='row'>
+        <div className="mb-4">
+          <label for="exampleFormControlInput1" className="form-label">Name</label>
+          <input type="text" className="form-control" id="exampleFormControlName" placeholder="Jon Doe"/>
+        </div>  
+        <div className="mb-4">
+          <label for="exampleFormControlInput1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlEmail" placeholder="name@example.com"/>
+        </div>
+        <div className="mb-4">
+          <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+          <textarea className="form-control" id="exampleFormControlText" rows="7"></textarea>
+        </div>
+      </div>
+
     </div>
   );
 }
